0d10d0b817f3bcf7c090dfd8b1913ca8c732f343
unknown <williammorenoprieto@gmail.com>
2019-06-01 04:03:54 -0500

    peticion

--- InsFinanciera/Models/Peticion.cs
+++ InsFinanciera/Models/Peticion.cs
@@ -7,17 +7,9 @@ namespace InsFinanciera.Models
 {
     public class Peticion
     {
-        public string TipoDoc { get; set; }
         public decimal Doc { get; set; }
         public string Pass { get; set; }
         public decimal Value { get; set; }
-        public decimal getDoc()
-        {
-            return this.Doc;
-        }
-        public string getPass()
-        {
-            return this.Pass;
-        }
+
     }
 }
